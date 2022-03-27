4af5123110fa182617fd17be12445be66578a735
Lars Rickert <dev@lars-rickert.de>
2022-03-27 12:30:19 +0200

    refactor: move app port to config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import { config } from './config';
 import errorMiddleware from './middleware/errors';
 import loggerMiddleware from './middleware/logger';
 import { createLogger } from './utils/logger';
 
 const app = express();
-const port = 3000;
 export const logger = createLogger();
 
 // routes have to been registered before middleware
@@ -16,4 +16,4 @@ app.use(router);
 router.use(loggerMiddleware);
 router.use(errorMiddleware);
 
-app.listen(port, () => console.log(`Running on http://localhost:${port}`));
+app.listen(config.app.port, () => console.log(`Running on http://localhost:${config.app.port}`));
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,7 @@
 export interface Config {
+  app: {
+    port: number;
+  };
   log: {
     /** Max size of the logfile in bytes */
     maxSize: number;
