a4cf5683c1d02f61424778ea0c606ac6aa84b3fd
Lars Rickert <dev@lars-rickert.de>
2022-03-27 12:22:56 +0200

    style: enable strict typescript

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,10 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import { Config } from './types/config';
-
-// load data from .env file to process.env
-dotenv.config();
 
 export const isProduction = process.env.NODE_ENV === 'production';
 
 export const config: Config = {
   log: {
-    // default to 2MB
-    maxSize: Number.parseInt(process.env.LOG_MAX_SIZE) || 1024 * 1024 * 2,
+    maxSize: Number.parseInt(process.env.LOG_MAX_SIZE ?? '') || 1024 * 1024 * 2,
   },
 };
